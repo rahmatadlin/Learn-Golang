5f8cc43fe34b7f1881ee746bfb54c21eafb6235a
rahmatadlin <rahmatadlin210@gmail.com>
2024-02-15 14:18:23 +0700

    4

---
+++ belajar-golang-dasar/closure.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	counter := 0
+
+	increment := func() {
+		fmt.Println("Increment")
+		counter++
+	}
+
+	increment()
+	increment()
+	increment()
+	increment()
+	increment()
+	increment()
+	increment()
+
+	fmt.Println(counter)
+}
